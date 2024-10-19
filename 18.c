1d51e0535d0bd26ea33a89c3984f5b4768ed6749
Ricardo Magalhães Parente Frota <ricardoparente58@gmail.com>
2024-10-19 16:01:50 -0300

    Update 18.c

--- 18.c
+++ 18.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
 
-int main(){
-    int i = 0, j = 0, s = 0, l = 0, e = 0, a = 0, r = 0;
-    
-    printf("largura do retangulo:");
-    scanf("%d",&r);
-    
-    printf("altura do retangulo:");
-    scanf("%d",&a);
+int main() {
+    printf("********\n");
+    printf("*      *\n");
+    printf("*      *\n");
+    printf("*      *\n");
+    printf("*      *\n");
+    printf("*      *\n");
+    printf("*      *\n");
+    printf("********\n\n");
 
-    for ( j = 0; j < r; j++){
-        for (i = 0; i < a; i++)
-        {
-            printf("*");
-        }
-        printf("\n");
-    }
-    printf("Digite o losango:");
-    scanf("%d", &l);
-    for ( i = 0; i < l; i++){
-        for (j = 0; j < l; j++){
-            if ((i+j < (l - 1)/2) || (j - i >= (l+1)/2) || (i + j >= l + (l-1)/2) || (i - j >= (l + 1)/2)){
-                printf(" ");
-            }else{
-                printf("*");
-            }
-        }
-        printf("\n");
-    }
-    
+    printf("   *****   \n");
+    printf(" *       * \n");
+    printf("*         *\n");
+    printf("*         *\n");
+    printf("*         *\n");
+    printf("*         *\n");
+    printf("*         *\n");
+    printf(" *       * \n");
+    printf("   *****   \n\n");
+
+    printf("     *     \n");
+    printf("    ***    \n");
+    printf("   *****   \n");
+    printf("     *     \n");
+    printf("     *     \n");
+    printf("     *     \n");
+    printf("     *     \n");
+    printf("     *     \n");
+    printf("     *     \n\n");
+
+    printf("    *    \n");
+    printf("   * *   \n");
+    printf("  *   *  \n");
+    printf(" *     * \n");
+    printf("*       *\n");
+    printf(" *     * \n");
+    printf("  *   *  \n");
+    printf("   * *   \n");
+    printf("    *    \n");
+
+    return 0;
 }
